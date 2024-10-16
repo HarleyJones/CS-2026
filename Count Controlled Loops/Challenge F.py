7116c9362970e7039edb0397c419e375e11130ca
Harley Jones <150591975+harleyjones@users.noreply.github.com>
2024-10-16 11:20:13 +0000

    ADD: Challenge F

--- Count Controlled Loops/Challenge F.py
+++ Count Controlled Loops/Challenge F.py
@@ -1,7 +1,7 @@
 num = int(input("Enter a number between 1 and 12: "))
 
-if 1 < num <= 12:
-    for i in range(12):
-        print((i + 1) * num)
+if 1 <= num <= 12:
+    for i in range(1, 12):
+        print(i * num)
 else:
     print("Not between 1 and 12")
